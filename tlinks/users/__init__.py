e2b61372c2e2e65c8993280ee345e694aab2672e
Raul Escamilla <raul.escamilla@asesoriait.com>
2020-05-01 22:03:06 -0600

    Routers moved to main package

--- tlinks/users/__init__.py
+++ tlinks/users/__init__.py
@@ -4,5 +4,21 @@
     ~~~~~~~~~~~~~~
     tlinks users package
 """
+from flask_login import current_user
+from ..services import users
+from ..router import route
+from flask import Blueprint
 
-from .routes import * 
+bp = Blueprint('users', __name__, url_prefix='/users')
+
+@route(bp, '/')
+def whoami():
+    """Returns the user instance of the currently authenticated user."""
+    return {'name': 'Raúl Escamilla'}
+    # return current_user._get_current_object()
+
+
+@route(bp, '/<user_id>')
+def show(user_id):
+    """Returns a user instance."""
+    return users.get_or_404(user_id)
