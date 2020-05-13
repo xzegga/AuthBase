6bf7dfdd473f8bd82a4d2d1b7579fd76a96ccae0
Raul Escamilla <raul.escamilla@asesoriait.com>
2020-05-13 16:15:23 -0600

    to_json added to token model

--- main/auth/models.py
+++ main/auth/models.py
@@ -28,4 +28,4 @@ class Token(TokenJsonSerializer, db.Model):
     
   @classmethod
   def get_blacklisted(cls):
-    return [{'jti': token.jti} for token in  cls.query.all()]
+    return [token.to_json() for token in  cls.query.all()]
