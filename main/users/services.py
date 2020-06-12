d838e8c5b67df54d45f6dc815fbfc37f4713a27d
Raúl Escamilla <raul.escamilla@asesoriait.com>
2020-06-11 18:51:00 -0600

    Update services.py

--- main/users/services.py
+++ main/users/services.py
@@ -2,42 +2,57 @@ from flask import current_app
 from ..core import Service, guard
 from .models import User
 
-
+CONFIRMATION_SENDER = current_app.config.get('PRAETORIAN_CONFIRMATION_SENDER')   
 
 class UsersService(Service):
   __model__ = User
 
+  def get_authenticated_user_id(self):
+    registration_token = guard.read_token_from_header()
+    user = guard.get_user_from_registration_token(registration_token) 
+    return user.id
+
+
+  def get_authenticated_user(self):
+    registration_token = guard.read_token_from_header()
+    user = guard.get_user_from_registration_token(registration_token) 
+    return user
+
 
   def signup(self, req):
     """
-      Registers a new user by parsing a POST request containing new user info
-    """    
-    if self.first(username=req['username']):
-      return {'Error': 'User exist.'}, 400
-    
+      Registers a new user by parsing a POST request containing new user info and
+      dispatching an email with a registration token
+    """  
     email = req.get('email', None)
+    req['username'] = email
+
+    if self.first(username=email):
+      return {'message': 'There is an user related with this email'}, 400
+    
+    # Hasing password to save encrypted
     req['password'] = guard.hash_password(req['password'])
 
     new_user = self.create(**req)
     guard.send_registration_email(
       email, 
       user=new_user,   
-      confirmation_sender = current_app.config.get('PRAETORIAN_CONFIRMATION_SENDER')      
+      confirmation_sender = CONFIRMATION_SENDER
     )
 
-    ret = {'message': 'successfully sent registration email to user {}'.format(
-        new_user.username
-    )}
+    ret = {'message': 'Email has been sent to {}, please confirm your email to activate your account'.format(
+        new_user.email
+    )}, 201
 
-    return ret, 201
-
+    return ret
+   
 
   def activate(self):
     """
-      Activate registered user througth email confirmation token
+      Finalizes a user registration with the token that they were issued in their
+      registration email
     """        
-    registration_token = guard.read_token_from_header()
-    user = guard.get_user_from_registration_token(registration_token)       
+    user = self.get_authenticated_user()     
     user.is_active = True
 
     self.update(user)
@@ -46,20 +61,20 @@ class UsersService(Service):
       user,
       firstname=user.first_name,
       lastname=user.last_name,
-    )}
+    )}, 200
 
-    return ret, 200
+    return ret
+
 
   def update_password(self, user, password):
     """
-    
+      Update user password in the database
     """
     user.password = guard.hash_password(password)
     self.update(user)
-    ret = {'message': 'Success'}
+    ret = {'message': 'Password updated successfully'}, 200
 
-    return ret, 200
-
+    return ret
 
 
 users = UsersService()
