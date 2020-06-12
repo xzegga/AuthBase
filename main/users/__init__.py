23698a31d5ab39148428943a386a04473d4b79ac
Raúl Escamilla <raul.escamilla@asesoriait.com>
2020-06-11 18:51:38 -0600

    Update __init__.py

--- main/users/__init__.py
+++ main/users/__init__.py
@@ -9,28 +9,19 @@ from .services import users
 from ..router import route
 from flask import Blueprint, request
 
-bp = Blueprint('users', __name__, url_prefix='/users')
+bp = Blueprint('users', __name__, url_prefix='/')
 
 @bp.route('/signup', methods=['POST'])
 def signup():
-  """
-  Registers a new user by parsing a POST request containing new user info and
-  dispatching an email with a registration token
-  """
   req = request.get_json()
   return users.signup(req)
 
 
 @bp.route('/confirm', methods=['POST'])
 def confirm():
-  """
-  Finalizes a user registration with the token that they were issued in their
-  registration email
-  """   
   return users.activate()
 
 
-@route(bp, '/')
+@route(bp, '/users')
 def whoami():
-    """Returns the user instance of the currently authenticated user."""    
     return users.all()
